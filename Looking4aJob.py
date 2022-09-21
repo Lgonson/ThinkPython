f4f82e9362daf6249c3bd754adc77658f2bda241
Lucas González Sonnenberg <93942563+Lgonson@users.noreply.github.com>
2022-09-21 14:37:04 +0200

    Add files via upload

---
+++ Looking4aJob.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 21 11:02:00 2022
+
+@author: Lucas911
+"""
+
+def print_three_times(bruce):
+    print(bruce)
+    print(bruce)
+    print(bruce)
+    
+amazon = "be free young padawan. "
+amazon2 = "i'm looking for a job"    
+    
+print_three_times(amazon2*1)
---
+++ chapter 2 - Mat excercises.py
@@ -0,0 +1,63 @@
+#Think Python
+print("Chapter 2 Math Ex.1")
+#Calcular el volumen de una esfera con radio r. 
+#Para calcular radio utilizamos la formula 
+#4/3*pi*r**3
+#r = 5
+Volume = (4/3*3.141592653589793)*(5**3)
+print(Volume)
+print("##############################################")
+
+
+print("Chapter 2 Math Ex. 2")
+CoverPriceBook = 24,95
+#disscount = 40 %
+Book_neto = 24.95*60/100
+print("Cover price neto is: ")
+print(Book_neto)
+print("60 Books neto are:")
+units = 60
+print(units*Book_neto)
+
+#Calculating shipping costs
+Shipping_Costs = 3 + (60*0.75)
+print("Shipping costs for 60 units are:")
+print(Shipping_Costs)
+
+print("Calculating costs...")
+print(898.2+48)
+
+print("############################################")
+print("------------")
+print("Chapter 2 Excercise 3")
+#Formula para pasar los minutos en porcentaje de hora:
+#print(minutos/60*100)
+
+#If I leave my house at 6:52
+print("6:52 in percentage:")
+Leave_Hour = (52/60*100)
+print(Leave_Hour)
+print("---------------")
+#I run 2 mile at 8:15
+print("2 Miles slow")
+print("minutes:")
+print(2*8.15)
+print("percentage:")
+Two_miles_slow = (16.3/60*100)
+print("---------------")
+#3 miles at 7:12
+print("3 Miles fast")
+print("minutes")
+print(3*7.12)
+print("percentage")
+Three_miles_fast = (21.36/60*100)
+print("-------------")
+print("sum percentage")
+Running_Time_percentage = (Two_miles_slow + Three_miles_fast)
+print(Running_Time_percentage)
+Running_Time_Hour = Running_Time_percentage*60/100
+print(Running_Time_Hour)
+Breakfast_Minutes = 37.66 -(60 - 52)
+print(Breakfast_Minutes)
+Breakfast_Time = ("7:30")
+print(Breakfast_Time)
